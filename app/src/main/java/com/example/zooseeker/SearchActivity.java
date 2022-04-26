291b881da964d3ffb6482769e28978614d176728
unknown <kpage@ucsd.edu>
2022-04-25 23:45:16 -0700

    added search bar

---
+++ app/src/main/java/com/example/zooseeker/SearchActivity.java
@@ -0,0 +1,39 @@
+package com.example.zooseeker;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.app.SearchManager;
+import android.content.Context;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Adapter;
+import android.widget.BaseAdapter;
+import android.widget.SearchView;
+
+public class SearchActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_search);
+
+        Intent intent = getIntent();
+        if (Intent.ACTION_SEARCH.equals(intent.getAction())) {
+            String query = intent.getStringExtra(SearchManager.QUERY);
+            search(query);
+        }
+
+
+
+    }
+
+    public void search(String query) {
+
+    }
+
+
+}
