919df67d616fdd062cbcfe51d192c61f3aa05805
Clair Ma <clairbma@gmail.com>
2022-04-26 23:43:26 -0700

    30-Created empty unit test file

---
+++ app/src/test/java/com/example/zooseeker/ShortestPathUnitTest.java
@@ -0,0 +1,12 @@
+package com.example.zooseeker;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class ShortestPathUnitTest {
+    @Test
+    public void addition_isCorrect() {
+        assertEquals(4, 2 + 2);
+    }
+}
