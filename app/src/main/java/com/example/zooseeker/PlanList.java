adca8871e32ad2a2dbc64ba001ef8d64fd74f91a
Franktheknight <shl059@ucsd.edu>
2022-04-29 00:08:32 -0700

    Adding PlanList data structure

---
+++ app/src/main/java/com/example/zooseeker/PlanList.java
@@ -0,0 +1,56 @@
+package com.example.zooseeker;
+
+import org.jgrapht.Graph;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class PlanList {
+    private List<Location> myList;
+    /*
+        this currLocationIndex will be referring to which location user is on in the list
+        this currLocation index will be updated when we call sort or when user manually put
+        select their current location Default to be 0 when the list is initialized.
+     */
+    private int currLocationIndex;
+    //private ZooMap zooMap;
+
+    public PlanList(String json_data) {
+        this.myList = new ArrayList<>();
+        //this.zooMap = new ZooMap(json_data);
+        this.currLocationIndex = 0;
+    }
+
+    public int currExhibitIndex(Exhibit curr) {
+        return myList.indexOf(curr);
+    }
+
+    public Boolean addLocation(Location e) {
+        return this.myList.add(e);
+    }
+
+    public Location getCurrentLocation() {
+        return this.myList.get(currLocationIndex);
+    }
+
+    /*
+        This method is to be implemented after the merge with the shortest path branch
+        it will use zooMap object to get the shortest path from current location to the
+        next location in the list
+     */
+//    public Graph<String, IdentifiedWeightedEdge> getPathToNextLocation() {
+//        Location curr = this.myList.get(currLocationIndex);
+//        String currId = curr.getId();
+//        Location next = this.myList.get(currLocationIndex + 1);
+//        String nextId = next.getId();
+//        return zooMap.getShortestPath(currId, nextId);
+//    }
+
+    public void advanceLocation() {
+        this.currLocationIndex++;
+    }
+
+
+    //toImplement sort
+
+}
