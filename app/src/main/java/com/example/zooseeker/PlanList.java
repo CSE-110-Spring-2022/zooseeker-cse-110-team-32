4176b48b14af7bd5edc56951cccc593cf5a13452
Franktheknight <shl059@ucsd.edu>
2022-05-01 01:07:30 -0700

    #51/implemented two elementary methods in PlanList class to save list and load list

--- app/src/main/java/com/example/zooseeker/PlanList.java
+++ app/src/main/java/com/example/zooseeker/PlanList.java
@@ -1,9 +1,16 @@
 package com.example.zooseeker;
 
+import android.content.Context;
+import android.content.SharedPreferences;
+
+import androidx.appcompat.app.AppCompatActivity;
+
 import org.jgrapht.Graph;
 
 import java.util.ArrayList;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
 public class PlanList {
     private List<Location> myList;
@@ -50,7 +57,47 @@ public class PlanList {
     public void advanceLocation() {
         this.currLocationIndex++;
     }
+    /*
+        These two methods are for saving and loading PlanList, at the moment because
+        sharedPreferences cannot store object, it only store strings. There is a way to doing
+        serialization but that would be pretty complicated to do. The other way involving
+        putting the name strings into a set into the preferences and to load them, we use
+        string name as query to fetch the data from the database
+        I am currently using the second way by converting the arraylist into a set, and then
+        put the ID into the set and retrieve the set in the loadList method
+     */
+    public void saveList(Context context) {
+        SharedPreferences preferences =
+                context.getSharedPreferences("List_File", Context.MODE_PRIVATE);
+        SharedPreferences.Editor editor = preferences.edit();
+        Set<String> saveList = new HashSet<>();
+        for (Location e : myList) {
+            saveList.add(e.getId());
+        }
+        editor.putStringSet("list", saveList);
+        editor.apply();
+    }
 
+    /*
+    Experimental stage: this function will be used to load the list from preferences, the list
+    stored in preferences are list of Location IDs, then this method will retrieve the location
+    object from the database and add them to the PlanList(aka the arraylist). Since the ordering
+    might be different, we will call sort method in this class to sort the arraylist.
+    As you can see outDao will be our future database and sort will be implemented in the future.
+     */
+//    public void loadList(Context context) {
+//        SharedPreferences preferences = context.getSharedPreferences("List_File",
+//                Context.MODE_PRIVATE);
+//        Set<String> retrieveList = preferences.getStringSet("list", null);
+//        ArrayList<Location> newList = new ArrayList<>();
+//        for (String LocId : retrieveList ) {
+//            Location item = outDao.get(LocId);
+//            newList.add(item);
+//        }
+//        this.myList = newList;
+//        this.sort();
+//
+//    }
 
     //toImplement sort
 
