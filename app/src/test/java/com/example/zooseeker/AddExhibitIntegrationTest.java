10b46c3ba7cd749f66e75c1d9ee269af833acda3
Clair Ma <clairbma@gmail.com>
2022-05-01 17:53:40 -0700

    #38/fix test names, add search test

--- app/src/test/java/com/example/zooseeker/AddExhibitIntegrationTest.java
+++ app/src/test/java/com/example/zooseeker/AddExhibitIntegrationTest.java
@@ -5,7 +5,6 @@ import static org.junit.Assert.assertNotNull;
 
 import android.widget.ListView;
 import android.widget.SearchView;
-import android.widget.TextView;
 
 import androidx.lifecycle.Lifecycle;
 import androidx.recyclerview.widget.RecyclerView;
@@ -16,24 +15,24 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 
 @RunWith(AndroidJUnit4.class)
-public class SearchIntegrationTest {
+public class AddExhibitIntegrationTest {
     ActivityScenario<SearchActivity> scenario = ActivityScenario.launch(SearchActivity.class);
 
     @Test
-    public void testSearch(){
+    public void testAdd(){
         scenario.moveToState(Lifecycle.State.CREATED);
 
         scenario.onActivity(activity -> {
             SearchView searchBar = activity.findViewById(R.id.search_bar);
-            searchBar.setQuery("Lions", true);
-            ListView searchView = activity.findViewById(R.id.search_list);
-            ZooData.VertexInfo searchLion = (ZooData.VertexInfo) searchView.getItemAtPosition(0);
-            assertNotNull(searchLion);
-            assertEquals("Lions", searchLion.name);
             searchBar.setQuery("fox", true);
+            ListView searchView = activity.findViewById(R.id.search_list);
             ZooData.VertexInfo searchFox = (ZooData.VertexInfo) searchView.getItemAtPosition(0);
             assertNotNull(searchFox);
             assertEquals("Arctic Foxes", searchFox.name);
+            searchView.performItemClick(searchView.getAdapter().getView(0, null, null), 0, 0);
+            RecyclerView displayView = activity.recyclerView;
+            DisplayListAdapter displayAdapter = (DisplayListAdapter) displayView.getAdapter();
+            assertEquals(1, displayAdapter.getItemCount());
         });
     }
 
