03f04a09c310f107d0ff790209da2073c4550c8f
clairma01 <38985660+clairma01@users.noreply.github.com>
2022-05-04 22:36:47 -0700

    #25/fixed junit use

--- app/src/test/java/com/example/zooseeker/SortPlanUnitTest.java
+++ app/src/test/java/com/example/zooseeker/SortPlanUnitTest.java
@@ -5,14 +5,17 @@ import static org.junit.Assert.assertEquals;
 import android.content.Context;
 
 import androidx.test.core.app.ApplicationProvider;
+import androidx.test.ext.junit.runners.AndroidJUnit4;
 
 import org.jgrapht.GraphPath;
 import org.junit.Test;
+import org.junit.runner.RunWith;
 
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
 
+@RunWith(AndroidJUnit4.class)
 public class SortPlanUnitTest {
     @Test
     public void allExhibits() {
