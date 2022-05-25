e4edb7247f917417d0f371121dbf1d7a930c7d37
Karen Page <kpage@ucsd.edu>
2022-05-24 21:50:53 -0700

    fixed integration tests

--- app/src/test/java/com/example/zooseeker/PreviousLocationIntegrationTest.java
+++ app/src/test/java/com/example/zooseeker/PreviousLocationIntegrationTest.java
@@ -7,21 +7,39 @@ import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertNotNull;
 import static org.junit.Assert.assertTrue;
 
+import android.content.Context;
 import android.widget.Button;
 import android.widget.ListView;
 import android.widget.SearchView;
 import android.widget.TextView;
 
 import androidx.lifecycle.Lifecycle;
+import androidx.room.Room;
 import androidx.test.core.app.ActivityScenario;
+import androidx.test.core.app.ApplicationProvider;
 import androidx.test.ext.junit.runners.AndroidJUnit4;
 
+import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
 @RunWith(AndroidJUnit4.class)
 public class PreviousLocationIntegrationTest {
     ActivityScenario<SearchActivity> scenario = ActivityScenario.launch(SearchActivity.class);
+    ExhibitDatabase testDb;
+    ExhibitDao todoListItemDao;
+
+    @Before
+    public void resetDatabase(){
+        Context context = ApplicationProvider.getApplicationContext();
+        testDb = Room.inMemoryDatabaseBuilder(context, ExhibitDatabase.class)
+                .allowMainThreadQueries().build();
+
+        ExhibitDatabase.injectExhibitDatabase(testDb);
+
+        todoListItemDao = testDb.exhibitDao();
+    }
+
     @Test
     public void displayDirectionsClickBackTest(){
         scenario.moveToState(Lifecycle.State.CREATED);
